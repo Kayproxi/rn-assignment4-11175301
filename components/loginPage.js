66744ee5a545a583fa192ce873e501825853c0eb
adikajanai <adikajanai@gmail.com>
2024-06-12 15:49:46 +0000

    login page component

---
+++ components/loginPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const Login = () => {
+    return(
+       <View>
+        <Text> My first RN component</Text>
+       </View>
+    );
+}
+export default Login;
