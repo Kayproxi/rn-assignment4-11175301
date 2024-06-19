d3284d2a6d6b8fa27b7ac1fbb91406124907cb72
adikajanai <adikajanai@gmail.com>
2024-06-19 08:56:52 +0000

    navigator

---
+++ appNavigator.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './components/loginPage';
+import Home from './components/homePage';
+
+const Stack = createStackNavigator();
+
+function AppNavigator() {
+return (
+    <NavigationContainer>
+     <Stack.Navigator initialRouteName="Login">
+       <Stack.Screen name="Login" component={Login} />
+       <Stack.Screen name="Home" component={Home} />
+     </Stack.Navigator>
+  </NavigationContainer>
+);
+}
+
+export default AppNavigator;
